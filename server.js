da4739f33ac40d871d9c46c573a9e71f41c1da54
Madandola james <jamesmadandola@gmail.com>
2026-07-11 10:15:48 +0100

    ;;

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { createClient } from '@supabase/supabase-js';
-
+import base62 from './utils';
+import { encodeIdToBase62 } from './utils/base62';
 
 dotenv.config();
 
@@ -35,7 +36,53 @@ app.get('/api/db-test', async (req, res) => {
     if(errror)
         return res.status(404).json({ error: error.message});
     res.json({message: "database connection error"})
-})
+});
+
+// shorten 
+app.post('/api/shorten', async (req, res ) => { 
+    const { url } = req.body;
+    if (!url) {
+        return res.status(400).json({ error: 'Please provide a URL to shorten.'});
+    }
+    try {
+        const { data: insertedData, error: insertError } = await supabase
+          .from('urls')
+          .insert([{ original_url: url }])
+          .select()
+          .single();
+
+        if (insertError) throw insertError;
+
+        const dbId = insertedData.id;
+        const shortCode = encodeIdToBase62(dbId);
+
+        const { data: updatedData, error: updateError } = await supabase
+        .from('urls')
+        .update({ short_code: shorCode })
+        .eq('id', dbId)
+        .select()
+        .single();
+
+        if (updateError) throw updateError;
+
+        const shorturl = `http:localhost:${PORT}/${shortCode}`;
+
+        res.status(201).json({ 
+            original_url: updatedData.original_url,
+            short_code: shortCode,
+            short_url: shorturl
+        });
+
+
+     } catch(error) {
+        console,log("Error creatingshort url:", error);
+        return res.status(500).json({ error: "Internal server error while shortening URL"});
+    }
+});
+
+
+
+
 
 app.listen(8000, () => { 
     console.log(`server running on ${PORT}`);
